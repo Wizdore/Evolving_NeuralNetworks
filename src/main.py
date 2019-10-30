52fa9312210caf34d8195287c38b9d658edb27ce
Bernt A. Eide <bernta1@msn.com>
2019-10-30 23:13:40 +0100

    - Updated the main entry point.
    - Added fitness.

--- src/main.py
+++ src/main.py
@@ -1,19 +1,17 @@
 import gym
 import time
+import ANN
+import numpy as np
 
 if __name__ == "__main__":
-    env = gym.make('CartPole-v0')
-    env.reset()
-    
-    done = False
-    iteration = 0
-    while(not done and iteration < 1000):
-        iteration += 1
-        env.render()
-        observation, reward, done, info = env.step(env.action_space.sample()) # take a random action
-        print(observation)
-        time.sleep(0.05)
-            
-    env.close()
+    results = []
+    with gym.make('CartPole-v0') as env:
+        for _ in range(50):
+            network = ANN.ANN(np.random.uniform(0.0, 1.0, size=29))
+            network.render(env) # simulate
+            results.append(network) # evaluate?
 
+        results = sorted(results, key=lambda x: x.fitness, reverse=True)
+        print("Best score:", results[0].fitness)
 
+        # Run again with the 50% best?

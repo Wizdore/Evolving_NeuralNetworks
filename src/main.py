9e042a16df42e159c92ab451b430646e1f75abe3
Bernt A Eide <bernta1@msn.com>
2019-10-31 17:08:10 +0100

    - Fixed typos.

--- src/main.py
+++ src/main.py
@@ -30,9 +30,8 @@ def mutate_all(state, rate):
             state[idx] = np.random.uniform(-1.0,1.0)
     return state
 
-def mutation(state, rate=0.01):
-    
-    return state_mod
+def mutation_singular(state, rate):
+    pass
 
 if __name__ == "__main__":
     """
@@ -47,16 +46,13 @@ if __name__ == "__main__":
     - WHAT ELSE ?
     """
     with gym.make('CartPole-v0') as env:
-        population = [ANN.ANN(np.random.uniform(0.0, 1.0, size=29)) for _ in range(50)] # Initial population
+        population = [ANN.ANN(np.random.uniform(0.0, 1.0, size=29)) for _ in range(40)] # Initial population
         for gen in range(15): # generation
             for network in population:
                 network.render(env) # simulate
-            
-                network.state[0]
             
             print(population)
             population = sorted(population, key=lambda x: x.fitness, reverse=True) # Sort population based on best fitness.
             print("Best score for generation {} is {}.".format(gen+1, population[0].fitness))
             # mutate , GA stuff, make children, modify initial population ? scrap 50% worst entries
-            
-            
+                        

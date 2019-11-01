d958b0671fa34c932e29eb2e0cd9554688443268
Bernt A Eide <bernta1@msn.com>
2019-11-01 15:16:34 +0100

    - Added using crossover plus mutation and retaining.

--- src/main.py
+++ src/main.py
@@ -1,22 +1,15 @@
 import gym
-import ANN
 import numpy as np
 
 class ANNResult:
     """
     A class which resembles the result when simulating a neural network.
     """
-    def __init__(self, state, inputs, lastStep):
+    def __init__(self, state):
         self.fitness = 0
         self.state = state
-        self.inputs = None
-        self.setInputs(inputs, lastStep)
-    
-    def getState(self):
-        pass
-
-    def setInputs(self, inputs, lastStep):
-        self.inputs = np.concatenate([inputs, [lastStep]])
+        self.lastObservation = None
+        self.lastOutput = None
 
 def crossover(parent1, parent2):
     """
@@ -24,48 +17,69 @@ def crossover(parent1, parent2):
     The two parents are chosen from the top performers in the generation.
     Returns a tuple containing child 1 and child 2 which has mixed values from parent 1 and 2.
     """
-    pivot = np.random.randint(0, len(parent1)) # Pick a random number.
+    pivot = np.random.randint(0, len(parent1.state)) # Pick a random number.
     # Create children.
-    child1 = parent1
-    child2 = parent2
+    child1 = parent1.state
+    child2 = parent2.state
     # Crossover data.
-    child1[:pivot] = parent2[:pivot]
-    child2[:pivot] = parent1[:pivot]
+    child1[:pivot] = parent2.state[:pivot]
+    child2[:pivot] = parent1.state[:pivot]
     
-    return (child1, child2)
+    return [ANNResult(child1), ANNResult(child2)]
 
-def mutate_all(state, rate=0.001):
+def mutate_all(children, rate=0.001):
     """
     Go through each element in the state and check if that element should be mutated with
     probability 'rate'.
     """
-    m = np.random.uniform(0.0,1.0,size=len(state))
-    for idx, _ in enumerate(state):
-        if rate >= m[idx]:
-            state[idx] = np.random.uniform(-1,1)
-    return state
+    for child in children:
+        m = np.random.uniform(0.0,1.0,size=len(child.state))
+        for idx, _ in enumerate(child.state):
+            if rate >= m[idx]:
+                child.state[idx] = np.random.uniform(-1,1)
 
-def mutation_singular(state, rate=0.001):
+def mutation_singular(children, rate=0.001):
     """
     Mutate single index (element) in state if mutation probability = TRUE.
     """
-    if rate >= np.random.uniform(0,1):
-        state[np.random.randint(0,len(state))] = np.random.uniform(-1,1)
-    return state
+    for child in children:
+        if rate >= np.random.uniform(0,1):
+            child.state[np.random.randint(0,len(child.state))] = np.random.uniform(-1,1)
 
 def activationFunction(x):
     return ((2/(1+np.exp(-0.5*x)))-1)
 
-def getOutput(inputs, state):
+def getOutput(ann):
+    state = ann.state
+    inputs = activationFunction(np.concatenate([ann.lastObservation, [ann.lastOutput]])) # Normalizing the input values
+
     hl_weight = state[0:20].reshape(5,4)       # First 20 elements are weights for input to hidden layer edges
     hl_bias = state[20:24]                     # Next 4 elements are for 4 nodes of hidden layer
     ol_weight = state[24:28].reshape(4,1)      # Next 4 elements are for 4 weights for hidden to output edges 
     ol_bias = state[-1]                     # Next 1 element is the bias of the output layer
 
-    inputs = activationFunction(inputs)         # Normalizing the input values
     hl_result = (np.matmul(inputs, hl_weight) + hl_bias)        # calc from input to hidden layer (is linear activation good enough?!)
     ol_result = (np.matmul(hl_result, ol_weight) + ol_bias)     # calc from Hidden to output layer
+
     return ol_result        # returning raw result of the network
+
+def retainAndKeepBest(population):
+    """
+    Removes bad performers, crossovers and mutates best performers.
+    Returns new population for the next gen.
+    """
+    population = sorted(population, key=lambda x: x.fitness, reverse=True) # Sort population based on best fitness.
+    print("Best score for generation {} is {}.".format(gen+1, population[0].fitness))
+    population = population[:(int(len(population)*0.5))] # keep top 50%
+    print(len(population))
+    for i in range(0, len(population), 2):
+        parent1 = population[i]
+        parent2 = population[i+1]
+        children = crossover(parent1,parent2)
+        mutate_all(children)
+        population.extend(children)
+    print(len(population))
+    return population
 
 def simulate(env, ann, isFirstTime = False):
     """
@@ -73,14 +87,14 @@ def simulate(env, ann, isFirstTime = False):
     """
     env.reset()
     done = False
-    fitness = 0
     while(not done):
         env.render()
-        observation, reward, done, _ = env.step(env.action_space.sample() if isFirstTime else getOutput(ann.inputs, ann.state))
-        ann.setInputs(observation, 0) # predict next action based on observation?
-        fitness += reward
-
-    ann.fitness = fitness
+        output = (env.action_space.sample() if isFirstTime else getOutput(ann))
+        print(output)
+        observation, reward, done, _ = env.step(output)
+        ann.lastObservation = observation
+        ann.lastOutput = output
+        ann.fitness += reward
 
 if __name__ == "__main__":
     """
@@ -95,12 +109,11 @@ if __name__ == "__main__":
     - WHAT ELSE ?
     """
     with gym.make('CartPole-v0') as env:
-        population = [ANN.ANN(np.random.uniform(-1.0, 1.0, size=29)) for _ in range(50)] # Initial population
-        for gen in range(10): # generation
-            for network in population:
-                network.render(env) # simulate
+        population = [ANNResult(np.random.uniform(-1,1,29)) for _ in range(40)] # Initial population
+        for gen in range(15): # generation
+            for ann in population:
+                simulate(env,ann,(gen <= 0))
 
-            population = sorted(population, key=lambda x: x.fitness, reverse=True) # Sort population based on best fitness.
-            print("Best score for generation {} is {}.".format(gen+1, population[0].fitness))
-            # mutate , GA stuff, make children, modify initial population ? scrap 50% worst entries
-                        
+            population = retainAndKeepBest(population)
+            for p in population:
+                print(p.state)
